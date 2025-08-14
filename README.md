9f722908a7076fc78b547054a2b25a0f128823f8
Deeping <140127894+Deeping415@users.noreply.github.com>
2025-08-14 12:54:52 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# android_device_msm8937_32go_i25
+谁爱编译谁编译吧,vendor Android.bp有点问题
+如需要用extract-files.sh提取供应商树 用git clone https://github.com/LineageOS/android_tools_extract-utils ${LineageOS}/tools/extract-utils
---
+++ setup-makefiles.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -e
+export USE_MAKEFILE=true
+DEVICE=msm8937_32go_i25
+VENDOR=xtc
+
+MY_DIR=$(dirname "${BASH_SOURCE[0]}")
+MY_DIR=$(cd "${MY_DIR}"; pwd -P)
+
+ANDROID_ROOT=$(cd "${MY_DIR}/../../.."; pwd)
+
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
+    exit 1
+fi
+source "${HELPER}"
+
+# Initialize the helper
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
+
+# Warning headers and guards
+write_headers
+
+write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Finish
+write_footers
